cd4632a3bd00434b78d51d65434b07f4884104ac
Kaustubh Wadagavi <kaustubhwadagavi@gmail.com>
2021-06-04 19:04:42 +0530

    Added the script and output file

--- FindBigFiles.sh
+++ FindBigFiles.sh
@@ -7,6 +7,6 @@ if [ "$#" -ne 2 ]
 then
         echo "Please enter 1.File extension and 2.File size through command line!!"
 else
-	sudo find / -type f -name '*.'zip'' -size +50M | xargs ls -ltr -h | awk '{print $5,$6,$7,$8,$9}'
+	sudo find / -type f -name '*.'zip'' -size +''$fileSize'' | xargs ls -ltr -h | awk '{print $5,$6,$7,$8,$9}'
 fi
 
